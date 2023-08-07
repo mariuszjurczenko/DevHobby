829070bfe25d1d745a8a97490375ae3674eea3ce
Mariusz Jurczenko <mariuszjurczenko@gmail.com>
2023-08-07 19:19:22 +0200

    3.3) Create Model

---
+++ DevHobby/Models/Entities/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace DevHobby.Models.Entities
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public ICollection<Course>? Courses { get; set; }
+    }
+}
