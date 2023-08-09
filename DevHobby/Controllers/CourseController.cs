9340543668cf9492c2c6d0729f079e541b44eaf8
Mariusz Jurczenko <mariuszjurczenko@gmail.com>
2023-08-09 17:28:32 +0200

    3.6) Dodanie kontrolera

---
+++ DevHobby/Controllers/CourseController.cs
@@ -0,0 +1,21 @@
+﻿using DevHobby.Models.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevHobby.Controllers
+{
+    public class CourseController : Controller
+    {
+        private readonly ICourseRepository _courseRepository;
+
+        public CourseController(ICourseRepository courseRepository)
+        {
+            _courseRepository = courseRepository;
+        }
+
+        public IActionResult List()
+        {
+            var result = _courseRepository.AllCourses;
+            return View(result);
+        }
+    }
+}
