a65e0bc5512c3529c13dae39028fb80f574d294a
Mariusz Jurczenko <mariuszjurczenko@gmail.com>
2023-08-09 15:24:29 +0200

    3.4) Dodanie Repozytorium

--- DevHobby/Program.cs
+++ DevHobby/Program.cs
@@ -1,6 +1,10 @@
+using DevHobby.Models.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<ICategoryRepository, MockCategoryRepository>();
+builder.Services.AddScoped<ICourseRepository, MockCourseRepository>();
 
 var app = builder.Build();
 
