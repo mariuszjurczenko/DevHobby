e240d3a50729fc2d380191f669b4d81cccdaadae
Mariusz Jurczenko <mariuszjurczenko@gmail.com>
2023-08-19 11:32:06 +0200

    6.6) Dodanie EmailTaghelper i strony kontakt

---
+++ DevHobby/Controllers/ContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevHobby.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
